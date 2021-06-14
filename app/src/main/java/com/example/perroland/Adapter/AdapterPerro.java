9ecdda3ad91685a1c29f9b325734e75c58a05434
DESKTOP-UIMQ3A5\MAIAMIMANA <braisregueiro7@gmail.com>
2021-06-14 21:21:07 +0200

    add Constructor perro

--- app/src/main/java/com/example/perroland/Adapter/AdapterPerro.java
+++ app/src/main/java/com/example/perroland/Adapter/AdapterPerro.java
@@ -1,4 +1,65 @@
 package com.example.perroland.Adapter;
 
-public class AdapterPerro {
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.perroland.Perro;
+import com.example.perroland.R;
+import com.example.perroland.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AdapterPerro extends RecyclerView.Adapter<AdapterPerro.PerroHolder>  {
+
+    private final Activity act;
+    private List<Perro> items;
+    private static final int layout = R.layout.item_detalleperro;
+
+    public AdapterPerro(Activity act, List<Perro> items) {
+        this.act = act;
+        this.items= items;
+    }
+
+    @NonNull
+    @Override
+    public PerroHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+
+        return new PerroHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PerroHolder holder, int position) {
+        final Perro item = items.get(position);
+
+        String texto = item.getNombrePerro();
+        holder.nombrePerro.setText(texto);
+    }
+
+    @Override
+    public int getItemCount() { return items.size();}
+
+
+    public class PerroHolder extends RecyclerView.ViewHolder {
+        TextView nombrePerro;
+        TextView razaPerro;
+        TextView edadPerro;
+        TextView nombreDueño;
+
+        public PerroHolder(@NonNull View itemView) {
+            super(itemView);
+            nombrePerro = (TextView) itemView.findViewById(R.id.item_nombre_perro);
+            razaPerro = (TextView) itemView.findViewById(R.id.item_raza_perro);
+            edadPerro = (TextView) itemView.findViewById(R.id.item_edad_perro);
+            nombreDueño = (TextView) itemView.findViewById(R.id.item_nombre_dueno);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/perroland/Perro.java
@@ -0,0 +1,58 @@
+package com.example.perroland;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Perro implements Serializable {
+    @Expose
+    @SerializedName("Perro")
+    private String nombrePerro;
+    @Expose
+    @SerializedName("RazaPerro")
+    private String razaPerro;
+    @Expose
+    @SerializedName("description")
+    private String description;
+
+    public String getNombrePerro() {
+        return nombrePerro;
+    }
+
+    public void setNombrePerro(String nombrePerro) {
+        this.nombrePerro = nombrePerro;
+    }
+
+    public String getRazaPerro() {
+        return razaPerro;
+    }
+
+    public void setRazaPerro(String razaPerro) {
+        this.razaPerro = razaPerro;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Map toMap() {
+
+        HashMap map = new HashMap();
+
+        map.put("Perro", this.nombrePerro);
+        map.put("Raza", this.razaPerro);
+        map.put("description", this.description);
+
+        return map;
+
+    }
+
+
+}
